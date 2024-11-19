e9e08b29a6f835e4ff2859d75939511fe3e6feb0
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2024-11-19 14:30:27 +0600

    Mastering The Core Concepts Of Mongoose

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+const app = express();
+const port = 3000;
+app.use(cors());
+
+//parser
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default app;
+
+console.log(process.cwd());
