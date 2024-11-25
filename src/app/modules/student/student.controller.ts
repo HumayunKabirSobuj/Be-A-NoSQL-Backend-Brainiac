7b71c0bce548ce953ec34985bd5550fbdcff1e68
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2024-11-25 15:45:39 +0600

    zod validation added

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,27 +1,16 @@
 import { Request, Response } from 'express';
 import { studentServices } from './student.service';
-import studentValidationSchemaWithJoi from './student.validation';
+// import studentValidationSchema from './student.zod.validation';
 const CreateStudent = async (req: Request, res: Response) => {
   try {
-    // Joi schema for Student
-
     // const studentData=req.body.student;
     const { student: studentData } = req.body;
 
-    //Data validation using Joi
-    const { error, value } =
-      studentValidationSchemaWithJoi.validate(studentData);
-    //will call service function to sent this data
-    const result = await studentServices.createStudentIntoDB(value);
-    // console.log(error, value);
-    if (error) {
-      res.status(500).json({
-        success: true,
-        message: 'Something went wrong',
-        error: error.details,
-      });
-    }
+    //data validation using zod
+    // const zodParseData = studentValidationSchema.parse(studentData);
 
+    //will call service function to sent this data
+    const result = await studentServices.createStudentIntoDB(studentData);
     // sent response
     res.status(200).json({
       success: true,
