7d8ccaa5f7316831146d5645294a65a30634d5c1
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2024-12-05 22:22:53 +0600

    part-3

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -19,12 +19,15 @@ const findLastStudentId = async () => {
     .lean();
 
   // 203001   0001
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 // need year, semester code and 4 digit number
 export const genarateStudentID = async (paylod: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  const currentId = (0).toString(); // 0000 --> by default
+  if (await findLastStudentId()) {
+    const lastStudent = await findLastStudentId();
+  }
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${paylod.year}${paylod.code}${incrementId}`;
 
