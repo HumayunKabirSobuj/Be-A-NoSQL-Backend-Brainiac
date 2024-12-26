5c777b283df88f09bb667dbeb063cfc25bca5cc2
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2024-12-26 12:07:06 +0600

    Semester Registration Interface, Model, Validation done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -7,6 +7,7 @@ import { UserRoutes } from '../modules/user/user.route';
 import { CourseRoutes } from '../modules/Course/course.route';
 import { FacultyRoutes } from '../modules/Faculty/faculty.route';
 import { AdminRoutes } from '../modules/Admin/admin.route';
+import { semesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.route';
 
 const router = Router();
 
@@ -43,6 +44,12 @@ const moduleRoutes = [
     path: '/courses',
     route: CourseRoutes,
   },
+  {
+    path:'/semester-registrations',
+    route:semesterRegistrationRoutes
+
+  }
+ 
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
